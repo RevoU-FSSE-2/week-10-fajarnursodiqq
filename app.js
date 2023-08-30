ec40d57936748dafe9ee8fe1c1d2ffd49ba86c56
fajarnursodiqq <fajarnursodiqq@gmail.com>
2023-08-31 00:25:46 +0700

    revisi

--- app.js
+++ app.js
@@ -1,79 +1,50 @@
+require("dotenv").config();
+
 const express = require("express");
+const databaseMiddleware = require("./middleware/databaseMiddleware.js");
+const authRouter = require("./routes/auth-route.js");
+const transferRouter = require("./routes/transfer-route.js");
+const authmiddleware = require("./middleware/authentication-middleware.js");
+const swaggerUi = require("swagger-ui-express");
+const yaml = require("yaml");
+const fs = require("fs");
+const OpenApiValidator = require("express-openapi-validator");
+const router = express.Router();
 const { MongoClient, ObjectId } = require("mongodb");
+
+const openApiPath = "doc/openapi.yaml";
+const file = fs.readFileSync(openApiPath, "utf8");
+const swaggerDocument = yaml.parse(file);
 
 const app = express();
 
 app.use(express.json());
-let db;
-(async () => {
-  const client = await new MongoClient("mongodb://127.0.0.1:27017").connect();
-  db = client.db("revoufajar");
-})();
 
-//CRUD
-app.post("/v1/users", async (req, res) => {
-  console.log(req.body);
-  const { username, email } = req.body;
-  const user = await db.collection("users").insertOne({ username, email });
-  res.status(200).json({
-    message: "success",
-    data: user,
+app.get("/", (req, res) => {
+  res.send("Transfer Request Management API");
+});
+
+app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: openApiPath,
+    validateRequests: true,
+  })
+);
+
+app.use(databaseMiddleware);
+
+app.use("/auth", authRouter);
+app.use("/transfer", authmiddleware, transferRouter);
+
+app.use((err, req, res, next) => {
+  console.log(err, `<=================== error ==================`);
+  res.status(err.status || 500).json({
+    message: err.message,
+    // errors: err.errors,
   });
 });
 
-app.get("/v1/users", async (req, res) => {
-  const users = await db
-    .collection("users")
-    .find({ is_deleted: { $exists: false } })
-    .toArray();
-  res.status(200).json({
-    message: "success",
-    data: users,
-  });
-});
-
-app.put("/v1/users/:id", async (req, res) => {
-  const { id } = req.params;
-  const { username, email } = req.body;
-  const user = await db
-    .collection("users")
-    .updateOne({ _id: new ObjectId(id) }, { $set: { username, email } });
-
-  res.status(200).json({
-    message: "success",
-    data: user,
-  });
-});
-
-app.delete("/v1/users/:id", async (req, res) => {
-  const { id } = req.params;
-  const user = await db
-    .collection("users")
-    .findOneAndUpdate(
-      { _id: new ObjectId(id) },
-      { $set: { is_deleted: true } }
-    );
-
-  res.status(200).json({
-    message: "success",
-  });
-});
-
-// app.delete("/v1/users/:id", async (req, res) => {
-//   const { id } = req.params;
-//   const { username, email } = req.body;
-//   const user = await db
-//     .collection("users")
-//     .deleteOne({ _id: new ObjectId(id) }, { $set: { username, email } });
-
-//   res.status(200).json({
-//     message: "success",
-//     data: user,
-//   });
-// });
-
-const port = 3000;
-
-app.listen(port, () => {
-  console.log("Running on port http://localhost:${port}");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
